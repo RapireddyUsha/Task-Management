5a48caca85475e4f9a6a6c078c8e5b80cd4886e8
RapireddyUsha <usharapireddy@gmail.com>
2025-06-12 16:11:25 +0530

    Delete models directory

--- models/teamModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const teamSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  designation: { type: String, required: true }
-}, { timestamps: true });
-
-export default mongoose.model('Team', teamSchema);
